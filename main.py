1dc4f7c936263572c7b0ee56b070af45d563f6c5
yashbhal <yashb1004@gmail.com>
2020-09-11 11:53:13 -0700

    minor changes + changed comments a bit

--- main.py
+++ main.py
@@ -9,36 +9,32 @@ def main():
     response = requests.get(url)
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
-    # selects all html td elements with class titleColumn
+    # gets all movie tags
     movietags = soup.select('td.titleColumn')
-    # selects the a tag inside the titleColumns
+    # seleects the actors list
     inner_movietags = soup.select('td.titleColumn a')
-    # gets ratings
+    # gets movie ratings
     rating_tags = soup.select('td.posterColumn span[name=ir]')
 
-    movietag0 = movietags[0]
-    innermovietag0 = inner_movietags[0]
-
     # year is the last element in the split. get_year gets all years in the movietags
-
     def get_year(movie_tag):
         moviesplit = movie_tag.text.split()
         year = moviesplit[-1]
         return year
 
-    years = [get_year(tag) for tag in movietags]  # gets all years
-
+    year = [get_year(tag) for tag in movietags]  # gets all years
     actors_list = [tag['title'] for tag in inner_movietags]  # gets all actors
-    titles = [tag.text for tag in inner_movietags]  # gets all movie titles
-    ratings = [float(value['data-value']) for value in rating_tags]
+    title = [tag.text for tag in inner_movietags]  # gets all movie titles
+    rating = [float(value['data-value'])
+              for value in rating_tags]  # gets all ratings
 
-    n_movies = len(titles)
+    n_movies = len(title)
 
     while(True):
         i = random.randrange(0, n_movies)
 
         print(
-            f'{titles[i]} {years[i]}, rating: {ratings[i]:.1f}, starring: {actors_list[i]}')
+            f'{title[i]} {year[i]}, rating: {rating[i]:.1f}, starring: {actors_list[i]}')
 
         user_input = input("Do you want another movie suggestion? (y/n)")
         if user_input != 'y':
@@ -46,4 +42,5 @@ def main():
 
 
 if __name__ == '__main__':
+    print("Here's a movie suggestion for you!")
     main()
